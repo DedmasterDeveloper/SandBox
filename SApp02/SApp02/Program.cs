6acfbee5dd51f9352220958a9f2f4d8945815f9e
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-01 22:22:22 +0300

    Сохранение

---
+++ SApp02/SApp02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 1;
+            double c = 1;
+            double x = new double();
+
+            Console.Write("Введите число X: ");
+            string s = Console.ReadLine();
+            x = Convert.ToDouble(s);
+
+            var f = Math.Pow(x,2) + b *x + c;
+            Console.WriteLine($"{a} * {x}^2 + {b} * {x} + {c} = {f}");
+            Console.WriteLine("Нажмите Enter для закрытия");
+            Console.ReadLine();
+        }
+    }
+}
