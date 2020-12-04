5821d4e6c7e57cbb35889637f98b3e5a907baa8a
DedmasterDeveloper <dedmaster.developer@yandex.ru>
2020-12-04 20:29:58 +0300

    Создание метода ferst(Ветвление if)

--- SApp03/SApp03/Program.cs
+++ SApp03/SApp03/Program.cs
@@ -10,6 +10,22 @@ namespace SApp03
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.ferst();
+        }
+
+        public void ferst ()
+        {
+            Console.WriteLine("Введите 2 целыъ числа: ");
+            Console.Write("Первое число: ");
+            var b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Второе число: ");
+            var c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Наибольшее число: {0}", b > c ? b : c);
+
+            Console.WriteLine("Для выхода нажмите Enter");
+            Console.ReadLine();
         }
     }
 }
