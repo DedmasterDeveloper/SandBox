8a4f1d9c3f666e86cf3f30d2bbb00d65fd3cdc08
DedmasterDeveloper <dedmaster.developer@yandex.ru>
2020-12-04 20:34:39 +0300

    Добавление метода second

--- SApp03/SApp03/Program.cs
+++ SApp03/SApp03/Program.cs
@@ -27,5 +27,27 @@ namespace SApp03
             Console.WriteLine("Для выхода нажмите Enter");
             Console.ReadLine();
         }
+
+        public void second()
+        {
+            int a = 45;
+
+            switch(a)
+            {
+                case 1:
+                    Console.WriteLine("a = 1");
+                    break;
+                case 2:
+                    Console.WriteLine("a = 2");
+                    break;
+                case 3:
+                    Console.WriteLine("a = 3");
+                    break;
+                default:
+                    Console.WriteLine("Default Value...");
+                    break;
+            }
+            Console.ReadLine();
+        }
     }
 }
