562097bb445279e9943c79cccd82319f606c74d9
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-08 21:29:19 +0300

    Добавлены классы

---
+++ SApp04/SApp06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyClass myClass1 = new MyClass();
+            MyClass myClass2 = myClass;
+            Console.WriteLine($"myClass равен myClass1 ? Ответ: {myClass.Equals(myClass1)}" );
+            Console.WriteLine($"myClass равен myClass2 ? Ответ: {myClass.Equals(myClass2)}");
+            Console.ReadLine();
+        }
+    }
+}
