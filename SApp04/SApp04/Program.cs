0571c1de8ddcf0b1cd8fd86d5e6ca80d237e3df8
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-08 20:29:27 +0300

    Добвлена проверка данных

--- SApp04/SApp04/Program.cs
+++ SApp04/SApp04/Program.cs
@@ -10,13 +10,37 @@ namespace SApp04
     {
         static void Main(string[] args)
         {
+
             int a = 12;
             int b = 1;
 
             ChangeSign(ref a, b);
 
             Console.WriteLine($"{a} ; {b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"{a} ; {b}");
+            bool f;
+            int c = new int();
+            do
+            {
+                Console.Write("Введите число: ");
+                //Проверка на тип данных
+                f = int.TryParse(Console.ReadLine(), out c);
+            }
+            while (!f);
+
+            Console.WriteLine($"Ваше числол: {c}");
+
             Console.ReadLine();
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
         }
 
         static void ChangeSign(ref int a, int b)
