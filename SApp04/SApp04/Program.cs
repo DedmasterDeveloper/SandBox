72245ed48f73ad9b3f008552fb3e3acad6c58e0e
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-08 20:13:16 +0300

    Добавление метода ChangeSign

---
+++ SApp04/SApp04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 1;
+
+            ChangeSign(ref a, b);
+
+            Console.WriteLine($"{a} ; {b}");
+            Console.ReadLine();
+        }
+
+        static void ChangeSign(ref int a, int b)
+        {
+            a = -a;
+            b = -b;
+        }
+    }
+}
