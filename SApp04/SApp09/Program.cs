6d317ba5291876f923d820379558e0b5186cf65e
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-11 21:55:37 +0300

    Добавлен чтение из файла, запись в массив и вывод массива

--- SApp04/SApp09/Program.cs
+++ SApp04/SApp09/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,42 @@ namespace SApp09
 {
     class Program
     {
-        //public static int[] LoadArrayFromFileV1(string fileName)
-        //{
+        public static int[] LoadArrayFromFileV1(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                StreamReader reader = new StreamReader(fileName);
+                var str =  reader.ReadLine();
+                int.TryParse(str, out int len);
 
-        //}
+                var arr = new int[len];
+
+                for(int i = 0; i < len; i++)
+                {
+                    if(int.TryParse(reader.ReadLine(), out int num))
+                    {
+                        arr[i] = num;
+                    }
+                }
+
+                return arr; 
+                
+            }
+            else
+                throw new Exception("Файл не найден");
+        }
 
         static void Main(string[] args)
         {
             var dir = AppDomain.CurrentDomain.BaseDirectory + "ArrayList.txt";
+            var arr = LoadArrayFromFileV1(dir);
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
