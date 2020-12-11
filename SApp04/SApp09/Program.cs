2e1d32f0589209171974906e6f908288676dcd9c
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-11 21:40:11 +0300

    Добавлен фаел с массивом

--- SApp04/SApp09/Program.cs
+++ SApp04/SApp09/Program.cs
@@ -8,8 +8,14 @@ namespace SApp09
 {
     class Program
     {
+        //public static int[] LoadArrayFromFileV1(string fileName)
+        //{
+
+        //}
+
         static void Main(string[] args)
         {
+            var dir = AppDomain.CurrentDomain.BaseDirectory + "ArrayList.txt";
         }
     }
 }
