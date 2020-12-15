451811cff8a999abb3058e8088021337559dd662
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-15 20:49:24 +0300

    Работа с строками

---
+++ SApp05/SApp02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+            s1 = "abs";
+            string s2 = "";
+            string s3 = string.Empty;
+
+            var s4 = @"Hellow world! 
+   Hello Home";
+            Console.WriteLine(s4);
+            var s5 = "C:\\Windows\\ABC\\text.txt";
+            var s6 = @"C:\Windows\ABC\text.txt";
+
+            string s7 = new string('a', 10);
+
+            string _s1 = "Simple";
+            string _s2 = "SimplE";
+
+            Console.WriteLine($"_s1 vs _s2 : {_s2.CompareTo(_s1)}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
